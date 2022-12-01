2033cfc5e3a05960ad3f4b1e06a4e929d0c281d2
fatalyty <alexandre.kahn@quadratic-labs.com>
2022-12-01 11:22:48 +0100

    Feat[test_members.py]: test_member_added

--- tests/integration/events/test_members.py
+++ tests/integration/events/test_members.py
@@ -6,7 +6,66 @@ from starknet_py.utils.data_transformer.data_transformer import CairoSerializer
 from .. import constants, utils
 
 
-async def test_updateMembers(
+async def test_member_added(
+    contract: Contract,
+    contract_events: dict,
+    client: AccountClient,
+    address=0x363B71D0029390D64F3458939B470E3E6293904D36510B,
+    delegateAddress=0x363B71D0029390D64F3458939B470E3E6293904D36510B,
+    shares=69,
+    loot=69,
+    jailed=0,
+    lastProposalYesVote=0,
+    onboardedAt=8638726,
+):
+    info = {
+        "address": address,
+        "delegateAddress": delegateAddress,
+        "shares": shares,
+        "loot": loot,
+        "jailed": jailed,
+        "lastProposalYesVote": lastProposalYesVote,
+        "onboardedAt": onboardedAt,
+    }
+
+    invoke_result = await contract.functions["Member_add_member_proxy"].invoke(
+        info=info,
+        max_fee=10**16,
+    )
+
+    await invoke_result.wait_for_acceptance()
+
+    transaction_hash = invoke_result.hash
+    transaction_receipt = await client.get_transaction_receipt(transaction_hash)
+
+    # Takes events from transaction receipt
+    events = transaction_receipt.events
+
+    # Takes an abi of the event which data we want to serialize
+    # We can get it from the contract abi
+    emitted_event_abi = contract_events["MemberAdded"]
+
+    # ProposalAdded.emit(id=info.id, title=info.title, link=info.link, type=info.type, submittedBy=info.submittedBy, submittedAt=info.submittedAt);
+
+    # Creates CairoSerializer with contract's identifier manager
+    cairo_serializer = CairoSerializer(
+        identifier_manager=contract.data.identifier_manager
+    )
+
+    # Transforms cairo data to python (needs types of the values and values)
+    python_data = cairo_serializer.to_python(
+        value_types=emitted_event_abi["data"], values=events[0].data
+    )
+
+    assert python_data.memberAddress == address
+    assert python_data.loot == loot
+    assert python_data.shares == shares
+    assert python_data.onboardedAt == onboardedAt
+
+    return transaction_receipt
+
+
+async def test_member_updated(
     contract: Contract,
     contract_events: dict,
     client: AccountClient,
@@ -16,7 +75,7 @@ async def test_updateMembers(
     loot=42,
     jailed=0,
     lastProposalYesVote=3,
-    onboardedAt=1,
+    onboardedAt=86348726,
 ):
     info = {
         "address": address,
